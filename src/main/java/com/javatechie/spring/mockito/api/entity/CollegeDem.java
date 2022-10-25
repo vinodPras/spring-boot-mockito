1a6f32c088eb99563d9aa07216fffaa291217c74
vinodPras <vinod.prasad99@gmail.com>
2022-10-26 00:24:36 +0530

    added college class

---
+++ src/main/java/com/javatechie/spring/mockito/api/entity/CollegeDem.java
@@ -0,0 +1,11 @@
+package com.javatechie.spring.mockito.api.entity;
+
+public class CollegeDem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
